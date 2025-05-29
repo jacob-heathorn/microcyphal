237cab7cf5b9f59fa16a47b27307dec36fc3c0cc
Jacob <jacob.heathorn@gmail.com>
2025-05-29 12:19:16 -0700

    Inherit ftl::DataFrame

--- firmware/include/cyphal/frame.hpp
+++ firmware/include/cyphal/frame.hpp
@@ -2,11 +2,31 @@
 
 #include <stdint.h>
 
+#include "ftl/data_frame.hpp"
+
 namespace cyphal {
 
 
-class Frame {
-  uint8_t version_;
-};
+// Cyphal data frame
+class Frame : private ftl::DataFrame {
+  public:
+    // Offsets into the raw frame
+    //
+    // TODO
+    static constexpr std::size_t kPayloadOffset = 0;
+  
+    // Construct with payload size
+    explicit Frame(std::size_t size) : ftl::DataFrame(size) {}
+    Frame() = default;
+  
+    using ftl::DataFrame::size;
+    using ftl::DataFrame::operator bool;
+  
+    // Access the UDP payload pointer
+    uint8_t* data() noexcept { return front() + kPayloadOffset; }
+    const uint8_t* data() const noexcept { return front() + kPayloadOffset; }
+  };
+  
+  static_assert(sizeof(Frame) == sizeof(ftl::DataFrame));
 
 }
--- test/native/hello_world.cpp
+++ test/native/hello_world.cpp
@@ -5,6 +5,14 @@
 int main() {
   std::cout << "Hello World!" << std::endl;
 
-  cyphal::Frame f{};
+  // Create an allocator for the data frame
+  static constexpr size_t POOL_MEMORY_SIZE = 16 * 1024;
+  uint8_t* buffer = new uint8_t[POOL_MEMORY_SIZE];
+  ftl::BumpAllocator allocator(buffer, POOL_MEMORY_SIZE);
+
+  // Initialize data frame class with the memory allocator.
+  ftl::DataFrame::initialize(allocator);
+
+  cyphal::Frame f(2);
   (void)f;
 }
