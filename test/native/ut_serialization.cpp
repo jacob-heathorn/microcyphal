90cfbaba81ff8118fecbf222a9961aae8405e334
Jacob <jacob.heathorn@gmail.com>
2025-06-02 12:56:04 -0700

    use c style dsdl generation

--- test/native/ut_serialization.cpp
+++ test/native/ut_serialization.cpp
@@ -2,8 +2,46 @@
 
 #include <cstring>
 
-#include "uavcan/node/Heartbeat_1_0.hpp"
+// Test the C version first
+extern "C" {
+#include "uavcan/node/Heartbeat_1_0.h"
+}
 
+TEST(HeartbeatCTest, SerializeDeserialize) {
+  // Test the C version of the heartbeat serialization
+  uavcan_node_Heartbeat_1_0 msg_orig;
+  memset(&msg_orig, 0, sizeof(msg_orig));
+  msg_orig.uptime = 0x12345678;
+  msg_orig.health.value = uavcan_node_Health_1_0_NOMINAL;
+  msg_orig.mode.value = uavcan_node_Mode_1_0_OPERATIONAL;
+  msg_orig.vendor_specific_status_code = 0xAB;
+
+  uint8_t raw_buf[uavcan_node_Heartbeat_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_];
+  memset(raw_buf, 0, sizeof(raw_buf));
+
+  // Serialize using C functions
+  size_t serialized_size = sizeof(raw_buf);
+  int32_t result = uavcan_node_Heartbeat_1_0_serialize_(&msg_orig, raw_buf, &serialized_size);
+  ASSERT_GE(result, 0) << "C serialization failed";
+  EXPECT_EQ(serialized_size, uavcan_node_Heartbeat_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_);
+
+  // Deserialize using C functions
+  uavcan_node_Heartbeat_1_0 msg_copy;
+  memset(&msg_copy, 0, sizeof(msg_copy));
+  size_t deserialized_size = serialized_size;
+  result = uavcan_node_Heartbeat_1_0_deserialize_(&msg_copy, raw_buf, &deserialized_size);
+  ASSERT_GE(result, 0) << "C deserialization failed";
+
+  // Verify round-trip correctness
+  EXPECT_EQ(msg_copy.uptime, msg_orig.uptime);
+  EXPECT_EQ(msg_copy.health.value, msg_orig.health.value);
+  EXPECT_EQ(msg_copy.mode.value, msg_orig.mode.value);
+  EXPECT_EQ(msg_copy.vendor_specific_status_code, msg_orig.vendor_specific_status_code);
+}
+
+// Comment out the C++ test for now due to nunavut offset bugs
+#if 0
+#include "uavcan/node/Heartbeat_1_0.hpp"
 
 using namespace uavcan::node;
 using nunavut::support::bitspan;
@@ -61,3 +99,4 @@ TEST(HeartbeatRoundTripTest, SerializeDeserialize) {
   EXPECT_EQ(msg_copy.mode.value, msg_orig.mode.value);                  // FAILS: gets wrong value  
   EXPECT_EQ(msg_copy.vendor_specific_status_code, msg_orig.vendor_specific_status_code);  // FAILS: gets wrong value
 }
+#endif
